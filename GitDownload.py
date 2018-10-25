2063611613fcb6ef920baae7e868fa8e597fc0d1
ryanmcvicker12 <ryanmcvicker12@gmail.com>
2018-10-25 12:28:04 -0400

    added more changes to tests

--- GitDownload.py
+++ GitDownload.py
@@ -1,11 +1,17 @@
 
-
+#git api?
 
 class gitDownload:
     def __init__(self):
         return
 
+    def set_account_info(self):
+        #method to be called in case HttpException
+
     def get_repo(self):
         return
 
-    
+    def create_repo(self):
+        #user should be able to create their own repo
+
+    def find_repo()
---
+++ Tests/test_voice.py
@@ -0,0 +1,7 @@
+#unittests with Voice class
+
+try:
+    from Henry.Web import Web
+
+except ImportError:
+    print("couldent import web")
